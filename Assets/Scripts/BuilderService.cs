7a2e3f86392e084f1020749e1850b7146af1f239
TerentevVlad <ter.vlad.ter@gmail.com>
2022-03-27 15:32:49 +0300

    Доработан билд скрипт

--- Assets/Scripts/BuilderService.cs
+++ Assets/Scripts/BuilderService.cs
@@ -1,7 +1,9 @@
 #if UNITY_EDITOR
 
 
+using System;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,35 +11,45 @@ namespace BuildAgent
 {
     public class BuilderService
     {
-        private static readonly string OutputProjectsFolder = "../../Build";
-    
-        static void BuildAndroid()
-        {
-            BuildTarget target = BuildTarget.Android;
-            BuildOptions options = BuildOptions.CompressWithLz4;
-            EditorUserBuildSettings.SwitchActiveBuildTarget(target);
-            // PlayerSettings.applicationIdentifier = Environment.GetEnvironmentVariable("AppBundle");
-            // PlayerSettings.Android.keystoreName = Environment.GetEnvironmentVariable("KeystoreName");
-            // PlayerSettings.Android.keystorePass = Environment.GetEnvironmentVariable("KeystorePassword");
-            // PlayerSettings.Android.keyaliasName = Environment.GetEnvironmentVariable("KeyAlias");
-            // PlayerSettings.Android.keyaliasPass = Environment.GetEnvironmentVariable("KeyPassword");
-
-            BuildPipeline.BuildPlayer(GetScenesFromEditorBuildSettings(), string.Format("{0}/{1}.apk" , OutputProjectsFolder, PlayerSettings.applicationIdentifier), target, options);
-        }
-    
+       
         static string[] GetScenesFromEditorBuildSettings()
         {
             var projectScenes = EditorBuildSettings.scenes;
             List<string> scenesToBuild = new List<string>();
             for (int i = 0; i < projectScenes.Length; i++)
             {
-                if (projectScenes[i].enabled) {
+                if (projectScenes[i].enabled)
+                {
                     scenesToBuild.Add(projectScenes[i].path);
                 }
             }
+
             return scenesToBuild.ToArray();
         }
+        [MenuItem("Build/Apk")]
+
+        public static void BuildApk()
+        {
+            var outdir = System.Environment.CurrentDirectory + "/BuildOutPutPath/Android";
+            var outputPath = Path.Combine(outdir, Application.productName + ".apk");
+            // Обработка папки
+            if (!Directory.Exists(outdir)) Directory.CreateDirectory(outdir);
+            if (File.Exists(outputPath)) File.Delete(outputPath);
+
+
+            string[] scenes = GetScenesFromEditorBuildSettings();
+            BuildPipeline.BuildPlayer(scenes, outputPath, BuildTarget.Android, BuildOptions.None);
+            if (File.Exists(outputPath))
+            {
+                Debug.Log("Build Success :" + outputPath);
+            }
+            else
+            {
+                Debug.LogException(new Exception("Build Fail! Please Check the log! "));
+            }
+        }
     }
+    
 }
 
 #endif
